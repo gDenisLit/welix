ae056d98509ab09f98b07eac47620291000572c1
Denis Lit <denislitviak@gmail.com>
2023-04-25 13:31:18 +0300

    add bullet-points section to home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
     <main class="home-view">
-        <div class="app-hero">
+        <section class="app-hero">
             <div class="overlay"></div>
             <img src="../assets/img/hero3.jpg" alt="" class="app-hero__background">
 
-            <section class="text-container">
+            <div class="text-container">
                 <h1 class="hero-header">
                     Create a website without limits
                 </h1>
@@ -16,7 +16,49 @@
                 <router-link class="hero-btn" to="/template">
                     Get started
                 </router-link>
-            </section>
-        </div>
+            </div>
+        </section>
+        <section class="bullet-points">
+            <div class="headers">
+                <h2>GROW YOUR</h2>
+                <h2>BUSINESS</h2>
+                <h2>ONLINE
+                    <router-link class="action" to="/template">
+                        Get started →
+                    </router-link>
+                </h2>
+            </div>
+            <div class="steps">
+                <div class="demo">
+                    Illustration
+                </div>
+                <div class="cards">
+                    <article v-for="(step, idx) in steps" :key="step.title">
+                        <h3>{{ idx + 1 }}</h3>
+                        <div>
+                            <h2>{{ step.title }}</h2>
+                            <h5>{{ step.desc }}</h5>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
     </main>
 </template>
+
+<script setup lang="ts">
+const steps = [
+    {
+        title: 'Create a website',
+        desc: 'Select from any of our industry-leading website templates, designer fonts, and color palettes that best fit your personal style and professional needs.'
+    },
+    {
+        title: 'Ecommerce made easy',
+        desc: 'Explore which tools you want to add—whether it’s setting up an online store, booking services, or adding your favorite third-party extensions.',
+    },
+    {
+        title: 'Market your business',
+        desc: 'Stand out in every inbox and social feed. On-brand email campaigns and social tools make it easy to retain customers and grow your audience.'
+    }
+]
+</script>
